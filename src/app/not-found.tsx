70bc7496945bf12ef9b4d9c0de1df0eddf27a6ab
Elwyn <elwyn_de_neve@hotmail.com>
2024-01-22 06:44:29 +0100

    changed not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,10 @@
+import Main from "@/components/main";
+import H1 from "@/components/h1";
+
 export default function NotFound() {
-  return <main>The page could not be found | 404</main>;
+  return (
+    <Main className="px-3 py-24 sm:px-9">
+      <H1> The page could not be found | 404</H1>
+    </Main>
+  );
 }
